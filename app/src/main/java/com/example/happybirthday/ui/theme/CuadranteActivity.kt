7018c87184d3b311c8c490bc4d12a0b96ebf48e3
LeoGMR20 <leo250703@gmail.com>
2022-10-28 18:26:14 -0400

    Commit 28/10/2022 17:25 (Leandro Morales) -> CuadranteActivity.kt

--- app/src/main/java/com/example/happybirthday/ui/theme/CuadranteActivity.kt
+++ app/src/main/java/com/example/happybirthday/ui/theme/CuadranteActivity.kt
@@ -3,13 +3,21 @@ package com.example.happybirthday.ui.theme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.happybirthday.R
 import com.example.happybirthday.ui.theme.ui.theme.HappyBirthdayTheme
 
 class CuadranteActivity : ComponentActivity() {
@@ -18,11 +26,8 @@ class CuadranteActivity : ComponentActivity() {
         setContent {
             HappyBirthdayTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting2("Android")
+                Surface(color = MaterialTheme.colors.background) {
+                    ComposeQuadrantApp()
                 }
             }
         }
@@ -30,14 +35,70 @@ class CuadranteActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting2(name: String) {
-    Text(text = "Hello $name!")
+fun ComposeQuadrantApp() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
+                title = stringResource(R.string.title_cuadrante1),
+                description = stringResource(R.string.cuadrante_texto_1),
+                backgroundColor = Color.Green,
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                title = stringResource(R.string.title_cuadrante2),
+                description = stringResource(R.string.cuadrante_texto_2),
+                backgroundColor = Color.Yellow,
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
+                title = stringResource(R.string.title_cuadrante3),
+                description = stringResource(R.string.cuadrante_texto_3),
+                backgroundColor = Color.Cyan,
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                title = stringResource(R.string.title_cuadrante4),
+                description = stringResource(R.string.cuadrante_texto_4),
+                backgroundColor = Color.LightGray,
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+private fun ComposableInfoCard(
+    title: String,
+    description: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview4() {
+fun ComposeQuadrantAppPreview() {
     HappyBirthdayTheme {
-        Greeting2("Android")
+        ComposeQuadrantApp()
     }
 }
